28011c53d31657e48759e404f47f87119f561cd2
Josh Bowden <JBowden@simplifycompliance.com>
2018-10-02 10:07:43 -0400

    Added hero text and animation

--- js/app.js
+++ js/app.js
@@ -1653,7 +1653,7 @@ particlesJS('particles-js',
           "speed": 3
         },
         "repulse": {
-          "distance": 200
+          "distance": 50
         },
         "push": {
           "particles_nb": 4
--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 console.log('started');
+
+// Create name typewriter
+(function () {
+    var i = 0;
+    var txt = 'Hi, I\'m Josh Bowden.';
+    var txt2 = 'A full stack web developer with an eye for design and a nack for marketing.';
+    var speed = 60;
+  
+    function typeWriter() {
+      if (i < txt.length) {
+        document.getElementById("header-one").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+      } else if (i < ( txt.length + txt2.length )) {
+        document.getElementById("header-two").innerHTML += txt2.charAt(i - txt.length);
+        i++;
+        setTimeout(typeWriter, speed);
+      } else {
+        $('#menu-btn').show();
+        $('#my-work-btn').fadeIn();
+      }
+    }
+  
+    typeWriter();
+  
+  }());
